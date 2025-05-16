6e0fc265565f7b7bfbce8f250f6234102389d0db
Subash Mahat <smahat@bulldogmedia.com>
2025-05-15 20:09:28 -0500

    fixed gitignore location

--- GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60ab738bf3131b16cd9d18ebed5ff81527bbfa7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a142ab540ab6eb3de44f207679c5e9864cc1f15c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
